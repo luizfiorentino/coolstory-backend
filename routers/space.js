e332e27ab836b13ddb91722c100d3c9429f30836
luizfiorentino <lgfiorentino@gmail.com>
2022-11-16 19:28:58 +0100

    PUT route (/spaces/:id) protected by authMiddleware added to update user's space

--- routers/space.js
+++ routers/space.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { sequelize } = require("../models");
-
+const authMiddleware = require("../auth/middleware");
 const Space = require("../models").space;
 const Story = require("../models").story;
 const router = new Router();
@@ -32,6 +32,27 @@ router.get("/spaces/:id", async (req, res, next) => {
   }
 });
 
+router.put("/spaces/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const spaceId = parseInt(req.params.id);
+    const { title, description, backgroundColor, color } = req.body;
+
+    const thisSpace = await Space.findByPk(spaceId);
+    if (!thisSpace) {
+      res.status(400).send("Character not found");
+    } else {
+      const spaceUpdated = await thisSpace.update({
+        title,
+        description,
+        backgroundColor,
+        color,
+      });
+      res.send(spaceUpdated);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 // router.post("/spaces", async (req, res, next) => {
 //   // title: `<MyName>'s space`
 //   // - description: null
